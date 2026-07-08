029b09a6566489a80cf1ac564f0664419c4966cf
muhammedrufaid <mohdrufaid7@gmail.com>
2026-07-08 17:26:19 +0400

    Update featured Jebel Ali Village properties constants to reflect new listings and preserve order. Remove outdated references and add new property IDs for accurate API responses.

--- src/constants/featuredJebelAliVillageProperties.js
+++ src/constants/featuredJebelAliVillageProperties.js
@@ -1,20 +1,20 @@
 /** Fixed featured listings for GET /properties/featured-jebel-ali-village (order preserved). */
 const FEATURED_JEBEL_ALI_VILLAGE_PROPERTY_REF_NOS = [
-  'RO-R-02378',
+  'RO-S-03260',
+  'RO-S-03077',
+  'RO-S-02990',
   'RO-S-02985',
+  'RO-R-03181',
   'RO-S-02986',
   'RO-S-02989',
-  'RO-S-02990',
   'RO-S-02991',
   'RO-S-02992',
   'RO-S-02993',
-  'RO-S-03077',
   'RO-S-03079',
-  'RO-R-03181',
-  'RO-S-03197',
-  'RO-S-03198',
-  'RO-S-03260',
   'RO-S-03461',
+  'RO-S-03198',
+  'RO-S-03197',
+  'RO-R-02378',
 ];
 
 module.exports = { FEATURED_JEBEL_ALI_VILLAGE_PROPERTY_REF_NOS };
